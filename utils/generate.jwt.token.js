d8e098e5127a5890de465495978b8e7ec6f44449
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-26 00:31:16 +0500

    Jwt generate Helper Function

--- utils/generate.jwt.token.js
+++ utils/generate.jwt.token.js
@@ -1,8 +1,7 @@
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/env.js";
 
-export const generateJwtToken = (userId, expiredAt) => {
-  return jwt.sign({ id: userId }, JWT_SECRET, { expiresIn: expiredAt });
+export const generateJwtToken = (userId, expiredAt, purpose = "auth", tokenVersion = 0) => {
+  return jwt.sign({ id: userId, purpose, tokenVersion }, JWT_SECRET, { expiresIn: expiredAt });
 };
-
 
