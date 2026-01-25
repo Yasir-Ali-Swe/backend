92e05177abcbdfab3998cc9d59b482abb5fba28b
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-25 23:24:14 +0500

    Backend Server Entry Point

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/db.connection.js";
+import { PORT } from "./config/env.js";
+import authRoutes from "./routes/auth.route.js";
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.use("/api/auth", authRoutes);
