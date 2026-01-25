ad87eab69b8135bc8b3717c62ebdb8bd14767efc
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-25 21:24:54 +0500

    Project Setup

---
+++ config/env.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+
+const PORT = process.env.PORT;
+const DB_CONNECTION = process.env.DB_CONNECTION;
+const JWT_SECRET = process.env.JWT_SECRET;
+const EMAIL = process.env.EMAIL;
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+
+export { PORT, DB_CONNECTION, JWT_SECRET, EMAIL, EMAIL_PASSWORD };
