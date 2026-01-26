5a55f474e64cf9dc7287fc189b099fe3f50530d0
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-26 23:58:36 +0500

    Client Middleware

---
+++ middlewares/client-middleware.js
@@ -0,0 +1,16 @@
+import { getUserFromToken } from "../utils/make-jwt.js";
+
+export const clientMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies?.authToken;
+    const user = await getUserFromToken(token);
+    if (user.role !== "client") {
+      return res.status(403).json({ message: "Access denied. Clients only." });
+    }
+    req.user = user;
+    req.userId = user._id;
+    next();
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message });
+  }
+};
