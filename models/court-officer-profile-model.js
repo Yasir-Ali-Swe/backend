158ec25c9970c49207a5c385e65b6c8f5281b1f0
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-26 23:36:25 +0500

    Court Officer Profile Model

---
+++ models/court-officer-profile-model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const CourtOfficerProfileSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      unique: true,
+      required: true,
+    },
+    courtId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Court",
+      required: true,
+    },
+    courtOfficerProfileId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserProfile",
+      required: true,
+    },
+    designation: { type: String, default: "Court Officer" },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("CourtOfficerProfile", CourtOfficerProfileSchema);
