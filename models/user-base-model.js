718841e78f8bb1a3608d87b74feb28bad10539e1
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-26 21:14:30 +0500

    User Base Model

---
+++ models/user-base-model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userBaseSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["client", "admin", "clerk", "lawyer", "court-officer"],
+      default: "client",
+    },
+    isVerified: { type: Boolean, default: false },
+    isProfileComplete: { type: Boolean, default: false },
+    tokenVersion: { type: Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+const UserBaseModel = mongoose.model("User", userBaseSchema);
+export default UserBaseModel;
