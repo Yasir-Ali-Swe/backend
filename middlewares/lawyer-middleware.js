e1e204a2e87bde4c12e89ff3c67f9f6074412b79
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-27 00:02:19 +0500

    Lawyer Middleware

---
+++ middlewares/lawyer-middleware.js
@@ -0,0 +1,16 @@
+import { getUserFromToken } from "../utils/make-jwt.js";
+
+export const lawyerMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies?.authToken;
+    const user = await getUserFromToken(token);
+    if (user.role !== "lawyer") {
+      return res.status(403).json({ message: "Access denied. Lawyers only." });
+    }
+    req.user = user;
+    req.userId = user._id;
+    next();
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message });
+  }
+};
