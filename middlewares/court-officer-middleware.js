1457c1fe6677064338459d1db2b8a4bbee790c8a
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-27 00:00:42 +0500

    Court Officer Middleware

---
+++ middlewares/court-officer-middleware.js
@@ -0,0 +1,18 @@
+import { getUserFromToken } from "../utils/make-jwt.js";
+
+export const courtOfficerMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies?.authToken;
+    const user = await getUserFromToken(token);
+    if (user.role !== "court_officer") {
+      return res
+        .status(403)
+        .json({ message: "Access denied. Court Officers only." });
+    }
+    req.user = user;
+    req.userId = user._id;
+    next();
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message });
+  }
+};
