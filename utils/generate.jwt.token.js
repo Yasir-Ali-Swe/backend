85203acb7b7ec56894132a33e9698356238e795a
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-26 20:13:35 +0500

    Delete the unwanted files

--- utils/generate.jwt.token.js
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config/env.js";
-
-export const generateJwtToken = (userId, expiredAt, purpose = "auth", tokenVersion = 0) => {
-  return jwt.sign({ id: userId, purpose, tokenVersion }, JWT_SECRET, { expiresIn: expiredAt });
-};
-
