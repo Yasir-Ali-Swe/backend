6fa668322a4fe79d72b6d5bc3a9772fa04e6e501
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-27 11:04:38 +0500

    Server main file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import { connectDB } from "./config/db-connection.js";
 import authRoutes from "./routes/auth-routes.js";
+import clientRoutes from "./routes/client-routes.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -24,3 +25,4 @@ app.listen(PORT, async () => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/client", clientRoutes);
