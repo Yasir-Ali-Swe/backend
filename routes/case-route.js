be112f803414350d4891af8918f1adc8d0014604
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-27 18:20:00 +0500

    Case Routes

--- routes/case-route.js
+++ routes/case-route.js
@@ -1,5 +1,6 @@
-import { lawyerDraftCase, lawyerUpateTheDraftCase, lawyerDeleteTheDraftCase, lawyerGetTheCaseById, lawyerGetAllTheCase, lawyerSubmitCase } from "../controllers/case-controller.js";
+import { lawyerDraftCase, lawyerUpateTheDraftCase, lawyerDeleteTheDraftCase, lawyerGetTheCaseById, lawyerGetAllTheCase, lawyerSubmitCase, clerkGetSubmitedCases, clerkRegisterCase } from "../controllers/case-controller.js";
 import { lawyerMiddleware } from "../middlewares/lawyer-middleware.js";
+import { clerkMiddleware } from "../middlewares/clerk-middleware.js";
 import express from "express";
 
 const router = express.Router();
@@ -9,7 +10,8 @@ router.delete("/delete-draft-case/:caseId", lawyerMiddleware, lawyerDeleteTheDra
 router.get("/get-case/:caseId", lawyerMiddleware, lawyerGetTheCaseById);
 router.get("/get-all-case", lawyerMiddleware, lawyerGetAllTheCase);
 router.post("/submit-case/:caseId", lawyerMiddleware, lawyerSubmitCase);
-
+router.get("/get-submited-cases", clerkMiddleware, clerkGetSubmitedCases);
+router.post("/register-case/:caseId", clerkMiddleware, clerkRegisterCase);
 
 
 export default router;
