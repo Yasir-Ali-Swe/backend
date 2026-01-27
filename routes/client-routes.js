d39865335e2b74b3247ff94935efb33bd6d4cfd9
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-27 13:21:49 +0500

    Client Routes

--- routes/client-routes.js
+++ routes/client-routes.js
@@ -2,8 +2,11 @@ import {
   createClientProfile,
   getClientProfile,
   updateClientProfile,
+  sendProposal,
+  getClientProposals,
 } from "../controllers/client-controller.js";
 import { clientMiddleware } from "../middlewares/client-middleware.js";
+import { isProfileComplete } from "../middlewares/profile-complete-middleware.js";
 import express from "express";
 
 const router = express.Router();
@@ -11,5 +14,12 @@ const router = express.Router();
 router.post("/create-profile", clientMiddleware, createClientProfile);
 router.get("/get-profile", clientMiddleware, getClientProfile);
 router.put("/update-profile", clientMiddleware, updateClientProfile);
+router.post(
+  "/send-proposal",
+  clientMiddleware,
+  isProfileComplete,
+  sendProposal
+);
+router.get("/get-proposals-sent", clientMiddleware, getClientProposals);
 
 export default router;
