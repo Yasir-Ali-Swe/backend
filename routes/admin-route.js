41a37ae66427af4a21e7bc528c197e0b706a2b3a
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-01-27 15:15:08 +0500

    Admin Routes

---
+++ routes/admin-route.js
@@ -0,0 +1,24 @@
+import { createProfile, adminUpdateHisProfile, adminGetHisProfile, adminCreateInternalUser, adminCreateCourt, getAllCourts, getCourtById, adminGetAllInternalUsers, adminGetInternalUserById } from "../controllers/admin-controller.js";
+import { adminMiddleware } from "../middlewares/admin-middleware.js";
+import express from "express";
+
+const router = express.Router();
+
+//admin Routes
+router.post("/create-profile", adminMiddleware, createProfile);
+router.put("/update-profile", adminMiddleware, adminUpdateHisProfile);
+router.get("/get-profile", adminMiddleware, adminGetHisProfile);
+
+//Court Routes
+router.post("/create-court", adminMiddleware, adminCreateCourt);
+router.get("/get-all-courts", adminMiddleware, getAllCourts);
+router.get("/get-court/:courtId", adminMiddleware, getCourtById);
+
+
+//admin internal user routes
+router.post("/create-internal-user", adminMiddleware, adminCreateInternalUser);
+router.get("/get-internal-users", adminMiddleware, adminGetAllInternalUsers)
+router.get("/get-internal-user/:internalUserId", adminMiddleware, adminGetInternalUserById)
+
+
+export default router;
